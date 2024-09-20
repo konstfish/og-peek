4146af8d5c1c8e95139a26200c8fda448355af4b
David Fischer <david@konst.fish>
2024-09-20 12:58:45 +0100

    feat(container): add dockerfile for handout & extend compose

--- test/producer.py
+++ test/producer.py
@@ -3,10 +3,12 @@ import json
 import time
 from random import randint
 
-
+"""
 r = redis.Redis(host='localhost', port=6379, db=0)
+# randomness
 for _ in range(5):
     url = f"https://example.com/{randint(1000, 9999)}"
     r.xadd("screenshot-urls", {"url": url})
 
     time.sleep(0.2)
+"""
